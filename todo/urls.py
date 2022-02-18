62f83c8a7143db1ad384512447137dec80de739f
kczub <kczub11@gmail.com>
2022-02-19 00:35:59 +0100

    CSS

--- todo/urls.py
+++ todo/urls.py
@@ -7,6 +7,7 @@ from .views import (
     create_view,
     update_view,
     delete_view,
+    confirm_completed_view,
     completed_view
 )
 
@@ -15,9 +16,10 @@ app_name = 'todo'
 urlpatterns = [
     path('', index_view, name='index'),
     path('create/', create_view, name='create'),
+    path('completed/', completed_view, name='completed'),
     path('profile/', profile_view, name='profile'),
-    path('<int:pk>/completed/', completed_view, name='completed'),
-    path('<int:pk>/delete/', delete_view, name='delete'),
+    path('<int:pk>/confirm-complete/', confirm_completed_view, name='confirm_completed'),
+    path('<int:pk>/confirm-delete/', delete_view, name='delete'),
     path('<int:pk>/', detail_view, name='detail'),
     path('<int:pk>/update/', update_view, name='update'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,13 +1,9 @@
 from django.contrib.auth.decorators import login_required 
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.conf import settings
 from django.shortcuts import render, redirect, get_object_or_404
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse
 
 from todo.models import Todo
 from todo.forms import TodoForm
-
-User = settings.AUTH_USER_MODEL
 
 
 def index_view(request):
@@ -20,7 +16,6 @@ def profile_view(request):
     qs = Todo.objects.filter(user__username=user)
     context = {
         'active': qs.filter(completed=False).order_by('-updated'),
-        'completed': qs.filter(completed=True)[:3],
     }
     return render(request, 'todo/profile.html', context)
 
@@ -47,7 +42,7 @@ def create_view(request):
         obj.save()
         return redirect(reverse('todo:profile'))
 
-    return render(request, 'todo/create-update.html', context)
+    return render(request, 'todo/create_update.html', context)
 
 
 @login_required
@@ -62,7 +57,7 @@ def update_view(request, pk):
         form.save()
         return redirect(todo_object)
 
-    return render(request, 'todo/create-update.html', context)
+    return render(request, 'todo/create_update.html', context)
 
 
 @login_required
@@ -79,7 +74,7 @@ def delete_view(request, pk):
 
 
 @login_required
-def completed_view(request, pk):
+def confirm_completed_view(request, pk):
     todo_object = get_object_or_404(Todo, pk=pk)
     context = {
         'todo': todo_object
@@ -89,4 +84,14 @@ def completed_view(request, pk):
         todo_object.save()
         return redirect('todo:profile')
 
+    return render(request, 'todo/confirm_completed.html', context)
+
+
+@login_required
+def completed_view(request):
+    user = request.user
+    qs = Todo.objects.filter(user__username=user)
+    context = {
+        'completed': qs.filter(completed=True).order_by('-updated')
+    }
     return render(request, 'todo/completed.html', context)
